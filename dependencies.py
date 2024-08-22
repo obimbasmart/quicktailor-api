376f780c0b8703b5e1e99f188e0e8631ddd29ba8
memmalino <memmalino#gmail.com>
2024-08-22 15:27:58 +0100

    merge successful

--- dependencies.py
+++ dependencies.py
@@ -1,10 +1,7 @@
 from database import SessionLocal
 from sqlalchemy.orm import Session
-<<<<<<< HEAD
-=======
 from src.users.models import User
 from src.tailors.models import Tailor
->>>>>>> af78e58bb7db916f01f9e423023ce4ad464af908
 
 def get_db():
     db: Session = SessionLocal()
@@ -18,11 +15,4 @@ def _utils_get_by_email(email: str, db: Session):
     user = db.query(User).filter(email==User.email).one_or_none()
     if not user:
         return db.query(Tailor).filter(email==Tailor.email).one_or_none()
-<<<<<<< HEAD
     return user
-
-
-
-=======
-    return user
->>>>>>> af78e58bb7db916f01f9e423023ce4ad464af908
